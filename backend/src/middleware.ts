2e6037e66144ca9a86bfa0e39ed1294884130e50
shivapranav07 <dasishivapranav@gmail.com>
2024-05-24 23:14:30 +0530

    firstcommit

---
+++ backend/src/middleware.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { verify } from "hono/jwt";
+
+
+export function initMiddleware(app:any) {    
+  // @ts-ignore
+  app.use('/api/v1/blog/*', async (c, next) => {
+      const header = c.req.header("authorization") || "";
+      // Bearer token => ["Bearer", "token"];
+      const token = header.split(" ")[1]
+      
+     // @ts-ignore
+      const response = await verify(token, c.env.JWT_SECRET)
+      if (response.id) {
+        next()
+      } else {
+        c.status(403)
+        return c.json({ error: "unauthorized" })
+      }
+    })
+    
+}
